3e38a9dd4efbd6f5e9c0c3a691bf1aa6b20d45eb
Vanessa Chu <v-chu.00@hotmail.com>
2017-11-09 22:17:48 -0800

    completed responsive navbar

--- js/myTestJs.js
+++ js/myTestJs.js
@@ -1,3 +1,14 @@
+// in mobile responsive mode, when hamburger is clicked, dropdown menu shows
+document.getElementById ("hamburger").addEventListener ("click", function(){
+    var x = document.getElementById('dropDownClick');
+    if(x.className === 'navmenu'){
+        x.className += " responsive";
+    }else{
+        x.className = 'navmenu'
+    }
+});
+
+// change active state of menu items when one is clicked
 function clickSingleA(a)
 {
     var items = document.querySelectorAll('.single.active');
